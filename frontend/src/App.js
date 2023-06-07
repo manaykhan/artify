7116b5dfa22d91e9799d56a695d6d3487417bcdf
Mahnoor Khan <manayyykhan123@gmail.com>
2023-06-07 19:23:03 +0500

    creating-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+      <a href = "/">Artify</a>
+      </header>
+      <main>
+        List Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
