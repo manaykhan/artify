d3bf6f6e473023502b20c5e1c01e7fe190711d4c
Mahnoor Khan <manayyykhan123@gmail.com>
2023-06-07 20:41:23 +0500

    added-products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,27 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
       <header>
-      <a href = "/">Artify</a>
+        <a href="/">Artify</a>
       </header>
       <main>
-        List Products
+        <h1>Featured Paintings</h1>
+        <div className="products">
+          {data.products.map((product) => (<div className="product" key={product.slug}>
+            <a href={'/product/${product.slug'}>
+              <img src={product.image} alt={product.name} />
+            </a>
+            <div className="product-info">
+              <a href={'/product/${product.slug'}>
+                <p>{product.name}</p>
+              </a>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to cart</button>
+            </div>
+          </div>))
+          }</div>
       </main>
     </div>
   );
---
+++ frontend/src/data.js
@@ -0,0 +1,135 @@
+const data = {
+    products:[
+        {
+            name: 'Girl Face Aesthetic Painting',
+            slug: 'girl-face-painting-decor',
+            category: 'Painting',
+            image: '/images/img1.jpg',
+            price: 100,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Couple Aesthetic Painting',
+            slug: 'couple-painting-decor',
+            category: 'Painting',
+            image: '/images/img2.jpg',
+            price: 100,
+            countInStock: 15,
+            rating: 4,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Line Art',
+            slug: 'line-art',
+            category: 'Painting',
+            image: '/images/img3.jpg',
+            price: 150,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Set of Paintings',
+            slug: 'set-of-paintings',
+            category: 'Painting',
+            image: '/images/img4.jpg',
+            price: 800,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Set of 3 Paintings',
+            slug: 'set-of-3-paintings',
+            category: 'Painting',
+            image: '/images/img5.jpg',
+            price: 450,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Boho Painting',
+            slug: 'boho-painting',
+            category: 'Painting',
+            image: '/images/img6.jpg',
+            price: 250,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Abstract Painting set of 2',
+            slug: 'abstract-painting',
+            category: 'Painting',
+            image: '/images/img7.jpg',
+            price: 200,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Boho Abstract set of 5',
+            slug: 'boho-abstract-set',
+            category: 'Painting',
+            image: '/images/img8.jpg',
+            price: 600,
+            countInStock: 15,
+            rating: 4.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: 'Set of Boho Prints',
+            slug: 'set-of-boho-prints',
+            category: 'Painting',
+            image: '/images/img9.jpg',
+            price: 500,
+            countInStock: 15,
+            rating: 3.5,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+
+        {
+            name: '3pcs Abstract Pattern',
+            slug: '3pcs-abstract-pattern',
+            category: 'Painting',
+            image: '/images/img10.jpg',
+            price: 350,
+            countInStock: 15,
+            rating: 4,
+            numReviews: 10,
+            description: 'desc',
+
+        },
+    ],
+};
+
+export default data;
